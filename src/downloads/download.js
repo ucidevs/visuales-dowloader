f3f751d928742ffd6123ba6ce57813c1c006eafa
_leodev <84308529+leonardof02@users.noreply.github.com>
2024-04-08 15:31:58 -0400

    feat(downloads): Download Subtitles

--- src/downloads/download.js
+++ src/downloads/download.js
@@ -42,12 +42,19 @@ const allSubsToDownload = [...allLinkElements].filter((a) => detectSubtitlesRege
 const allThumbnails = [...allLinkElements].filter((a) => detectPhotoFilesRegex.test(a.href));
 
 // Get URLs from filtered video Link Elements
-const itemsToDownload = allVideoLinksToDownload.map((link) => link.href);
+const urlsToDownload = allVideoLinksToDownload.map((link) => link.href);
 
-/** List of Downloaded items
+/** List of Subs items to download
  * @type {DownloadItem[]}
  */
-const fileNamesToDownload = allVideoLinksToDownload.map((link) => {
+const subsItemsForDownload = allSubsToDownload.map((link) => {
+  return { name: link.textContent, isSelected: true, link: link.href };
+});
+
+/** List of Video items to download
+ * @type {DownloadItem[]}
+ */
+const itemsToDownload = allVideoLinksToDownload.map((link) => {
   return { name: link.textContent, isSelected: false, link: link.href };
 });
 
@@ -72,21 +79,20 @@ function renderAllThumbnails() {
 function selectElement(event) {
   const index = event.target.getAttribute("index");
   if (index === null || index === undefined) return;
-  fileNamesToDownload[index].isSelected = !fileNamesToDownload[index].isSelected;
+  itemsToDownload[index].isSelected = !itemsToDownload[index].isSelected;
   renderView();
 }
 
 selectAllButton.onclick = () => {
-  if (fileNamesToDownload.some((file) => file.isSelected === true))
-    fileNamesToDownload.forEach((file, index) => (fileNamesToDownload[index].isSelected = false));
-  else fileNamesToDownload.forEach((file, index) => (fileNamesToDownload[index].isSelected = true));
+  if (itemsToDownload.some((file) => file.isSelected === true))
+    itemsToDownload.forEach((file, index) => (itemsToDownload[index].isSelected = false));
+  else itemsToDownload.forEach((file, index) => (itemsToDownload[index].isSelected = true));
   renderView();
 };
 
 function renderView() {
-
   // render HTML for Items to Download
-  const itemsToDownload = fileNamesToDownload.map(
+  const itemsToDownloadHTML = itemsToDownload.map(
     (link, index) => /*html*/ `<div class="download-item ${
       link.isSelected === true ? "checked" : ""
     }" index="${index}">
@@ -94,7 +100,7 @@ function renderView() {
     <h4>${link.name}</h4>
   </div>`
   );
-  itemsView.innerHTML = itemsToDownload.join("");
+  itemsView.innerHTML = itemsToDownloadHTML.join("");
 
   // Set all events
   document.querySelectorAll(".download-item").forEach((item) => {
@@ -102,13 +108,13 @@ function renderView() {
   });
 
   // Render or not the download button
-  if (fileNamesToDownload.some((file) => file.isSelected === true))
+  if (itemsToDownload.some((file) => file.isSelected === true))
     downloadButton.style.display = "block";
   else downloadButton.style.display = "none";
 }
 
 // Check for videos and render Custom View
-if (itemsToDownload.length > 0) {
+if (urlsToDownload.length > 0) {
   pageTitle.insertAdjacentElement("afterend", itemsView);
   pageTitle.insertAdjacentElement("afterend", selectAllButton);
   document.querySelector("table").remove();
@@ -121,8 +127,11 @@ if (itemsToDownload.length > 0) {
  * Envía al service worker los datos del archivo a descargar
  */
 function startDownloads() {
-  const downloads = fileNamesToDownload.filter((file) => file.isSelected);
+  const downloads = itemsToDownload.filter((file) => file.isSelected);
   const URL = document.querySelector("h1").textContent.split("/");
   const path = URL.pop();
   chrome.runtime.sendMessage({ action: "download", downloads: downloads, path });
+  if (confirm("¿Desea descargar los subtítulos?")) {
+    chrome.runtime.sendMessage({ action: "download", downloads: subsItemsForDownload, path });
+  }
 }
